548d792cda90b9203942ed403f5c516b2c052650
liuhuiling <liuhuiling4@huawei.com>
2020-09-17 15:49:39 +0800

    modify and add models. add ut.
    modify and add models. add ut.
    
    Issue-ID: I1QGTR
    
    Signed-off-by: liuhuiling <liuhuiling4@huawei.com>

--- src/main/java/org/edgegallery/atp/utils/JSONUtil.java
+++ src/main/java/org/edgegallery/atp/utils/JSONUtil.java
@@ -5,17 +5,30 @@ import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONUtil {
+
 	private static final ObjectMapper MAPPER = new ObjectMapper();
 
+	/**
+	 * transfer object to String.
+	 * 
+	 * @param obj obj
+	 * @return String type variable
+	 * @throws IOException
+	 */
 	public static String marshal(Object obj) throws IOException {
 		return MAPPER.writeValueAsString(obj);
 	}
 
-	public static <T> T unMarshal(String obj, Class<T> type) throws IOException {
-		return MAPPER.readValue(obj, type);
-	}
-
-	public static void main(String args[]) throws IOException {
-
+	/**
+	 * transfer String type to special model type.
+	 * 
+	 * @param <T>
+	 * @param src  souce String
+	 * @param type target type
+	 * @return target type model
+	 * @throws IOException
+	 */
+	public static <T> T unMarshal(String src, Class<T> type) throws IOException {
+		return MAPPER.readValue(src, type);
 	}
 }
