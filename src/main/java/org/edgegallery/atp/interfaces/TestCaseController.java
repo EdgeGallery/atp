60a02c3073f3c3b22e7f6e8f715babe0abcb07b2
liuhuiling <liuhuiling4@huawei.com>
2020-11-19 15:50:46 +0800

    mock role
    mock role
    
    Issue-ID: I1TDH6
    
    Signed-off-by: liuhuiling <liuhuiling4@huawei.com>

--- src/main/java/org/edgegallery/atp/interfaces/TestCaseController.java
+++ src/main/java/org/edgegallery/atp/interfaces/TestCaseController.java
@@ -17,7 +17,6 @@ package org.edgegallery.atp.interfaces;
 import java.util.List;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.servicecomb.provider.rest.common.RestSchema;
 import org.edgegallery.atp.model.task.TaskRequest;
 import org.edgegallery.atp.model.testcase.TestCase;
@@ -58,10 +57,7 @@ public class TestCaseController {
             @ApiResponse(code = 500, message = "resource grant " + "error", response = String.class)})
     // @PreAuthorize("hasRole('GUEST')")
     public ResponseEntity<List<TestCase>> getAllTestCases(@QueryParam("type") String type) {
-        if (!StringUtils.isEmpty(type)) {
-            CommonUtil.lengthCheck(type);
-        }
-
+        CommonUtil.lengthCheck(type);
         return testCaseService.getAllTestCases(type);
     }
 
