266df367a3bcae5cbf29b078f4fd588cd42eccf3
liuhuiling <liuhuiling4@huawei.com>
2020-11-27 14:54:35 +0800

    remove deploy dependency app
    remove deploy dependency app
    
    Issue-ID: I1TDH6
    
    Signed-off-by: liuhuiling <liuhuiling4@huawei.com>

--- src/main/java/org/edgegallery/atp/schedule/testcase/sandbox/InstantiateAppTestCase.java
+++ src/main/java/org/edgegallery/atp/schedule/testcase/sandbox/InstantiateAppTestCase.java
@@ -1,14 +1,20 @@
 package org.edgegallery.atp.schedule.testcase.sandbox;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
+import org.apache.commons.io.FileUtils;
 import org.edgegallery.atp.constant.Constant;
 import org.edgegallery.atp.constant.ExceptionConstant;
 import org.edgegallery.atp.model.testcase.TestCaseResult;
 import org.edgegallery.atp.schedule.testcase.TestCaseAbs;
 import org.edgegallery.atp.utils.CommonUtil;
+import org.edgegallery.atp.utils.file.FileChecker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
@@ -16,6 +22,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import com.google.gson.JsonArray;
@@ -59,68 +66,66 @@ public class InstantiateAppTestCase extends TestCaseAbs {
             }
         };
 
-        // StringBuffer appInstanceList = new StringBuffer();
-        // // analysis dependency app.
-        // Stack<Map<String, String>> dependencyAppList = new Stack<Map<String, String>>();
-        // CommonUtil.dependencyCheckSchdule(filePath, dependencyAppList, context);
-        //
-        // LOGGER.info("dependencyAppList: {}", dependencyAppList);
-        //
-        // // create instantiate
-        // List<String> failedAppName = new ArrayList<String>();
-        // dependencyAppList.forEach(map -> {
-        // InputStream inputStream =
-        // CommonUtil.downloadAppFromAppStore(map.get(Constant.APP_ID), map.get(Constant.PACKAGE_ID),
-        // context);
-        // // analysis response and get csar file, get csar file path
-        // String dependencyFilePath = new
-        // StringBuilder().append(FileChecker.getDir()).append(File.separator)
-        // .append("temp").append(File.separator).append(map.get(Constant.APP_ID)).append(Constant.UNDER_LINE)
-        // .append(map.get(Constant.PACKAGE_ID)).append(".csar").toString();
-        // LOGGER.warn("dependencyFilePath: {}", dependencyFilePath);
-        // File file = new File(dependencyFilePath);
-        // try {
-        // FileUtils.copyInputStreamToFile(inputStream, file);
-        // Map<String, String> pkgInfo = CommonUtil.getPackageInfo(dependencyFilePath);
-        // ResponseEntity<String> responsePM =
-        // CommonUtil.uploadFileToAPM(dependencyFilePath, context, getMecHost(context), pkgInfo);
-        // if (null == responsePM || !(HttpStatus.OK.equals(responsePM.getStatusCode())
-        // || HttpStatus.ACCEPTED.equals(response.getStatusCode()))) {
-        // LOGGER.error("uploadFileToAPM failed, response: {}", responsePM);
-        // }
-        // JsonObject jsonObjectDen = new JsonParser().parse(responsePM.getBody()).getAsJsonObject();
-        // Map<String, String> appInfoMap = new HashMap<String, String>() {
-        // {
-        // put(Constant.APP_NAME, pkgInfo.get(Constant.APP_NAME));
-        // put(Constant.APP_ID, jsonObjectDen.get("appId").getAsString());
-        // put(Constant.PACKAGE_ID, jsonObjectDen.get("appPackageId").getAsString());
-        // }
-        // };
-        //
-        // String instanceId = CommonUtil.createInstanceFromAppo(context, appInfoMap, getMecHost(context));
-        // if (null == instanceId) {
-        // failedAppName.add(map.get(Constant.APP_NAME));
-        // } else {
-        // appInstanceList.append(instanceId).append(Constant.COMMA);
-        // }
-        //
-        // } catch (IOException e) {
-        // String msg = "copy input stream to file failed.";
-        // LOGGER.error(msg);
-        // throw new IllegalArgumentException(msg);
-        // }
-        // });
-        //
-        // LOGGER.info("appInstanceList: {}", appInstanceList.toString());
-        //
-        // context.put(Constant.DEPENDENCY_APP_INSTANCE_ID, appInstanceList.toString());
+        StringBuffer appInstanceList = new StringBuffer();
+        // analysis dependency app.
+        Stack<Map<String, String>> dependencyAppList = new Stack<Map<String, String>>();
+        CommonUtil.dependencyCheckSchdule(filePath, dependencyAppList, context);
+
+        LOGGER.info("dependencyAppList: {}", dependencyAppList);
+
+        // create instantiate
+        List<String> failedAppName = new ArrayList<String>();
+        dependencyAppList.forEach(map -> {
+            InputStream inputStream =
+                    CommonUtil.downloadAppFromAppStore(map.get(Constant.APP_ID), map.get(Constant.PACKAGE_ID), context);
+            // analysis response and get csar file, get csar file path
+            String dependencyFilePath = new StringBuilder().append(FileChecker.getDir()).append(File.separator)
+                    .append("temp").append(File.separator).append(map.get(Constant.APP_ID)).append(Constant.UNDER_LINE)
+                    .append(map.get(Constant.PACKAGE_ID)).append(".csar").toString();
+            LOGGER.warn("dependencyFilePath: {}", dependencyFilePath);
+            File file = new File(dependencyFilePath);
+            try {
+                FileUtils.copyInputStreamToFile(inputStream, file);
+                Map<String, String> pkgInfo = CommonUtil.getPackageInfo(dependencyFilePath);
+                ResponseEntity<String> responsePM =
+                        CommonUtil.uploadFileToAPM(dependencyFilePath, context, getMecHost(context), pkgInfo);
+                if (null == responsePM || !(HttpStatus.OK.equals(responsePM.getStatusCode())
+                        || HttpStatus.ACCEPTED.equals(response.getStatusCode()))) {
+                    LOGGER.error("uploadFileToAPM failed, response: {}", responsePM);
+                }
+                JsonObject jsonObjectDen = new JsonParser().parse(responsePM.getBody()).getAsJsonObject();
+                Map<String, String> appInfoMap = new HashMap<String, String>() {
+                    {
+                        put(Constant.APP_NAME, pkgInfo.get(Constant.APP_NAME));
+                        put(Constant.APP_ID, jsonObjectDen.get("appId").getAsString());
+                        put(Constant.PACKAGE_ID, jsonObjectDen.get("appPackageId").getAsString());
+                    }
+                };
+
+                String instanceId = CommonUtil.createInstanceFromAppo(context, appInfoMap, getMecHost(context));
+                if (null == instanceId) {
+                    failedAppName.add(map.get(Constant.APP_NAME));
+                } else {
+                    appInstanceList.append(instanceId).append(Constant.COMMA);
+                }
+
+            } catch (IOException e) {
+                String msg = "copy input stream to file failed.";
+                LOGGER.error(msg);
+                throw new IllegalArgumentException(msg);
+            }
+        });
+
+        LOGGER.info("appInstanceList: {}", appInstanceList.toString());
+
+        context.put(Constant.DEPENDENCY_APP_INSTANCE_ID, appInstanceList.toString());
 
         // some dependence app instantiate failed.
-        // if (!CollectionUtils.isEmpty(failedAppName)) {
-        // return setTestCaseResult(Constant.FAILED,
-        // ExceptionConstant.INSTANTIATE_DEPENDENCE_APP_FAILED.concat(failedAppName.toString()),
-        // testCaseResult);
-        // }
+        if (!CollectionUtils.isEmpty(failedAppName)) {
+            return setTestCaseResult(Constant.FAILED,
+                    ExceptionConstant.INSTANTIATE_DEPENDENCE_APP_FAILED.concat(failedAppName.toString()),
+                    testCaseResult);
+        }
 
         // instantiate original app
         String appInstanceId = CommonUtil.createInstanceFromAppo(context, appInfo, getMecHost(context));
--- src/main/java/org/edgegallery/atp/schedule/testcase/sandbox/UninstantiateAppTestCase.java
+++ src/main/java/org/edgegallery/atp/schedule/testcase/sandbox/UninstantiateAppTestCase.java
@@ -1,6 +1,10 @@
 package org.edgegallery.atp.schedule.testcase.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 import org.edgegallery.atp.constant.Constant;
 import org.edgegallery.atp.constant.ExceptionConstant;
 import org.edgegallery.atp.model.testcase.TestCaseResult;
@@ -8,6 +12,7 @@ import org.edgegallery.atp.schedule.testcase.TestCaseAbs;
 import org.edgegallery.atp.utils.CommonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 
 /**
  * terminate app instance.
@@ -21,25 +26,25 @@ public class UninstantiateAppTestCase extends TestCaseAbs {
     @Override
     public TestCaseResult execute(String filePath, Map<String, String> context) {
         String appInstanceId = context.get(Constant.APP_INSTANCE_ID);
-        // String dependencyInstanceList = context.get(Constant.DEPENDENCY_APP_INSTANCE_ID);
-        //
-        // List<String> failedInstance = new ArrayList<String>();
-        // if (StringUtils.isNotEmpty(dependencyInstanceList)) {
-        // String[] dependencyInstanceArray =
-        // dependencyInstanceList.substring(dependencyInstanceList.length() - 1).split(Constant.COMMA);
-        // Arrays.stream(dependencyInstanceArray).forEach(instance -> {
-        // if (!CommonUtil.deleteAppInstance(instance, context)) {
-        // failedInstance.add(instance);
-        // }
-        // });
-        // }
+        String dependencyInstanceList = context.get(Constant.DEPENDENCY_APP_INSTANCE_ID);
 
-        // if (!CollectionUtils.isEmpty(failedInstance)) {
-        // LOGGER.error("some instance not terminate successfully.");
-        // return setTestCaseResult(Constant.FAILED,
-        // ExceptionConstant.UNINSTANTIATE_DEPENDENCE_APP_FAILED.concat(failedInstance.toString()),
-        // testCaseResult);
-        // }
+        List<String> failedInstance = new ArrayList<String>();
+        if (StringUtils.isNotEmpty(dependencyInstanceList)) {
+            String[] dependencyInstanceArray =
+                    dependencyInstanceList.substring(dependencyInstanceList.length() - 1).split(Constant.COMMA);
+            Arrays.stream(dependencyInstanceArray).forEach(instance -> {
+                if (!CommonUtil.deleteAppInstance(instance, context)) {
+                    failedInstance.add(instance);
+                }
+            });
+        }
+
+        if (!CollectionUtils.isEmpty(failedInstance)) {
+            LOGGER.error("some instance not terminate successfully.");
+            return setTestCaseResult(Constant.FAILED,
+                    ExceptionConstant.UNINSTANTIATE_DEPENDENCE_APP_FAILED.concat(failedInstance.toString()),
+                    testCaseResult);
+        }
 
         return CommonUtil.deleteAppInstance(appInstanceId, context)
                 ? setTestCaseResult(Constant.SUCCESS, Constant.EMPTY, testCaseResult)
