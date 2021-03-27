fa91745f29a4c5a161f40771b1b87810474719ac
liuhuiling <liuhuiling4@huawei.com>
2021-03-27 14:49:51 +0800

    compliance test case skip vm package
    compliance test case skip vm package
    
    Issue-ID: I1TDH6
    
    Signed-off-by: liuhuiling <liuhuiling4@huawei.com>

--- src/main/resources/testCase/Yaml Description File Validation.java
+++ src/main/resources/testCase/Yaml Description File Validation.java
@@ -1,5 +1,8 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.zip.ZipEntry;
@@ -8,10 +11,15 @@ import java.util.zip.ZipFile;
 public class YamlDescriptionFileValidation {
     private static final String YAML_FILE_NOT_EXISTS = "there is no yaml file in APPD/Definition/ dir.";
     private static final String INNER_EXCEPTION = "inner exception, please check the log.";
+    private static final String VM = "vm";
+    private static final String SUCCESS = "success";
 
     public String execute(String filePath, Map<String, String> context) {
         try {
             Thread.sleep(200);
+            if (VM.equalsIgnoreCase(getAppType(filePath))) {
+                return SUCCESS;
+            }
         } catch (InterruptedException e1) {
         }
         try (ZipFile zipFile = new ZipFile(filePath)) {
@@ -31,4 +39,34 @@ public class YamlDescriptionFileValidation {
 
         return YAML_FILE_NOT_EXISTS;
     }
+
+    /**
+     * get app_type
+     * 
+     * @param filePath filePath
+     * @return appType
+     */
+    private String getAppType(String filePath) {
+        try (ZipFile zipFile = new ZipFile(filePath)) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                if (entry.getName().split("/").length == 1 && entry.getName().endsWith(".mf")) {
+                    try (BufferedReader br = new BufferedReader(
+                            new InputStreamReader(zipFile.getInputStream(entry), StandardCharsets.UTF_8))) {
+                        String line = "";
+                        while ((line = br.readLine()) != null) {
+                            // prefix: path
+                            if (line.trim().startsWith("app_class")) {
+                                return line.split(":")[1].trim();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
 }
