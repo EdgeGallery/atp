4c9c60943d5b3867356fb1428f063f13742adda5
twilight0620 <liuhuiling4@huawei.com>
2022-01-10 10:49:12 +0800

    modify ut error
    modify ut error
    
    Issue-ID: I1TDH6
    
    Signed-off-by: liuhuiling <liuhuiling4@huawei.com>

--- src/main/java/org/edgegallery/atp/interfaces/advice/GlobalExceptionConvert.java
+++ src/main/java/org/edgegallery/atp/interfaces/advice/GlobalExceptionConvert.java
@@ -16,6 +16,7 @@ package org.edgegallery.atp.interfaces.advice;
 
 import java.io.FileNotFoundException;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.Response;
 import org.edgegallery.atp.utils.exception.FileNotExistsException;
 import org.edgegallery.atp.utils.exception.IllegalRequestException;
@@ -24,9 +25,11 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingMatrixVariableException;
 import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 @ControllerAdvice
 public class GlobalExceptionConvert {
@@ -34,7 +37,9 @@ public class GlobalExceptionConvert {
     /**
      * Handle Exception.
      *
-     * @return
+     * @param request request
+     * @param e exception
+     * @return rest return
      */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
@@ -49,24 +54,11 @@ public class GlobalExceptionConvert {
     }
 
     /**
-     * handle bad request exception.
-     *
-     * @param request request
-     * @param e exception
-     * @return restReturn
-     */
-    private RestReturn badRequestResponse(HttpServletRequest request, Exception e) {
-        return RestReturn.builder().code(Response.Status.BAD_REQUEST.getStatusCode())
-            .error(Response.Status.BAD_REQUEST.getReasonPhrase()).message(e.getMessage()).path(request.getRequestURI())
-            .build();
-    }
-
-    /**
      * Handler IllegalArgumentException.
      *
      * @param request request
      * @param e exception
-     * @return RestReturn
+     * @return rest return
      */
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseBody
@@ -79,7 +71,7 @@ public class GlobalExceptionConvert {
      *
      * @param request request
      * @param e exception
-     * @return RestReturn
+     * @return rest return
      */
     @ExceptionHandler(value = IllegalRequestException.class)
     @ResponseBody
@@ -90,11 +82,82 @@ public class GlobalExceptionConvert {
     }
 
     /**
+     * Handler AccessDeniedException.
+     *
+     * @param request request
+     * @param e exception
+     * @return rest return
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseBody
+    public RestReturn accessDeniedException(HttpServletRequest request, AccessDeniedException e) {
+        return RestReturn.builder().code(Response.Status.FORBIDDEN.getStatusCode())
+            .error(Response.Status.FORBIDDEN.getReasonPhrase()).message(e.getMessage()).path(request.getRequestURI())
+            .build();
+    }
+
+    /**
+     * Handle RuntimeException.
+     *
+     * @param request request
+     * @param e exception
+     * @return rest return
+     */
+    @ExceptionHandler(value = RuntimeException.class)
+    @ResponseBody
+    public RestReturn runtimeException(HttpServletRequest request, RuntimeException e) {
+        return RestReturn.builder().code(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
+            .error(Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase()).message(e.getMessage())
+            .path(request.getRequestURI()).build();
+    }
+
+    /**
+     * Handle ConstraintViolationException.
+     *
+     * @param request request
+     * @param e exception
+     * @return rest return
+     */
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    @ResponseBody
+    public RestReturn constraintViolationException(HttpServletRequest request, ConstraintViolationException e) {
+        return badRequestResponse(request, e);
+    }
+
+    /**
+     * Handle MissingServletRequestParameterException.
+     *
+     * @param request request
+     * @param e exception
+     * @return rest return
+     */
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    @ResponseBody
+    public RestReturn missingServletRequestParameterException(HttpServletRequest request,
+        MissingServletRequestParameterException e) {
+        return badRequestResponse(request, e);
+    }
+
+    /**
+     * Handle MissingServletRequestPartException.
+     *
+     * @param request request
+     * @param e exception
+     * @return rest return
+     */
+    @ExceptionHandler(value = MissingServletRequestPartException.class)
+    @ResponseBody
+    public RestReturn missingServletRequestPartException(HttpServletRequest request,
+        MissingServletRequestPartException e) {
+        return badRequestResponse(request, e);
+    }
+
+    /**
      * Handle FileNotFoundException.
      *
      * @param request request
      * @param e exception
-     * @return RestReturn
+     * @return rest return
      */
     @ExceptionHandler(value = FileNotFoundException.class)
     @ResponseBody
@@ -121,17 +184,15 @@ public class GlobalExceptionConvert {
     }
 
     /**
-     * Handler AccessDeniedException.
+     * bad request response.
      *
      * @param request request
      * @param e exception
-     * @return restReturn
+     * @return rest return
      */
-    @ExceptionHandler(AccessDeniedException.class)
-    @ResponseBody
-    public RestReturn accessDeniedException(HttpServletRequest request, AccessDeniedException e) {
-        return RestReturn.builder().code(Response.Status.FORBIDDEN.getStatusCode())
-            .error(Response.Status.FORBIDDEN.getReasonPhrase()).message(e.getMessage()).path(request.getRequestURI())
+    private RestReturn badRequestResponse(HttpServletRequest request, Exception e) {
+        return RestReturn.builder().code(Response.Status.BAD_REQUEST.getStatusCode())
+            .error(Response.Status.BAD_REQUEST.getReasonPhrase()).message(e.getMessage()).path(request.getRequestURI())
             .build();
     }
 }
